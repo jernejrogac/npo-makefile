e25c51c4f648d534290734a2e12f98feecf512f3
user <jernej.rogac@student.um.si>
2026-03-22 11:26:19 +0000

    Add basic preprocessor directive

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define g 9.81
+#define g 9.81    
 
 int funkcija(int x) {
     return x + x;
@@ -8,7 +8,13 @@ int funkcija(int x) {
 
 int main() {
     int y = funkcija(2);
+
+    #ifdef TEST
+        printf("To je test");
+    #endif
+
     printf("Rezultat: %d\n", y);
     printf("g = %.2f m/s²\n", g);
+
     return 0;
 }
