80a00735e328dcc84828843fbffb9197f9cca1bc
jernejrogac <jernej.rogac@student.um.si>
2026-05-03 20:47:27 +0200

    Removed unused files from testing

--- main.py
+++
@@ -1,9 +0,0 @@
-G = 9.81
-
-def funkcija(x):
-    return x + x
-
-if __name__ == "__main__":
-    y = funkcija(2)
-    print(f"Rezultat: {y}")
-    print(f"g = {G} m/s²")
--- test_main.py
+++
@@ -1,5 +0,0 @@
-import main
-
-def test_funkcija():
-    assert main.funkcija(1) == 2
-    assert main.funkcija(0) == 0
