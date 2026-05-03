3b9a8296203fe3194491c9c7065bdb1813a1f745
jernejrogac <jernej.rogac@student.um.si>
2026-05-03 15:10:08 +0000

    Update main.c for testing

--- main.c
+++ main.c
@@ -6,18 +6,3 @@ int funkcija(int x) {
     return x + x;
 }
 
-#ifndef TESTING
-int main() {
-    int y = funkcija(2);
-    #ifdef TEST
-        printf("[TEST MODE]\n");
-        printf("Debug: funkcija(2) = %d\n", y);
-        printf("Debug: g = %.2f\n", g);
-    #else
-        printf("Normalni nacin delovanja\n");
-    #endif
-    printf("Rezultat: %d\n", y);
-    printf("g = %.2f m/s²\n", g);
-    return 0;  
-}
-#endif
